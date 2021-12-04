faf61738f985889c4ffb39b13904164ec26bc638
AgaRosiak <93795221+AgaRosiak@users.noreply.github.com>
2021-12-04 11:10:39 +0100

    Create main.py
    kalkulator

---
+++ main.py
@@ -0,0 +1,11 @@
+import kalkulator
+
+suma = (kalkulator.dodaj(4,5))
+pomnoz = (kalkulator.pomnoz(2,9))
+
+dwiesumy = kalkulator.dodaj(2,2) + kalkulator.dodaj(7,1)
+
+print(suma)
+print(pomnoz)
+print(dwiesumy)
+
